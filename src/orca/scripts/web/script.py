0765e50f7bf51c82f2ede79fa359e028c6f9c0aa
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-04-24 18:18:51 +0200

    New upstream version 3.33.1

--- src/orca/scripts/web/script.py
+++ src/orca/scripts/web/script.py
@@ -630,7 +630,7 @@ class Script(default.Script):
         """Updates the context and presents the find results if appropriate."""
 
         text = self.utilities.queryNonEmptyText(obj)
-        if not (text and text.getNSelections()):
+        if not (text and text.getNSelections() > 0):
             return
 
         document = self.utilities.getDocumentForObject(obj)
@@ -785,6 +785,11 @@ class Script(default.Script):
 
         if self._inFocusMode and obj and obj.getRole() == pyatspi.ROLE_RADIO_BUTTON:
             msg = "WEB: Staying in focus mode due to role of %s" % obj
+            debug.println(debug.LEVEL_INFO, msg, True)
+            return True
+
+        if self._inFocusMode and self.utilities.isWebAppDescendant(obj):
+            msg = "WEB: Staying in focus mode because we're inside a web application"
             debug.println(debug.LEVEL_INFO, msg, True)
             return True
 
